a5187d13c0145758db3f755a3cb35415797d2133
Isabella Garrido <igarrido.7420@unimar.edu.ve>
2024-10-23 16:14:16 -0400

    Seccion tramos y tarifas

--- Proyecto1.pas
+++ Proyecto1.pas
@@ -1,12 +1,19 @@
 program Proyecto1;
-uses crt;
+
+uses crt,SysUtils;
+
 var
+//Areglos
+estacionesStr: array[1..4] of string;
+estaciones: array[1..4] of integer;
 //Menu inicial
 opMenuIni: string;
 //Contadores
 ContAsientosDisponibles: integer;
 //Datos del Usuario
 name, CI: string;
+//Tramos
+opMenuTramos: Integer;
 //Cantidad de boletos comprados 1 vez
 boletos: string;
 opMenuCantBoletosstr: string;
@@ -16,12 +23,20 @@ precioboleto: integer;
 errorCantBoletos: integer;
 //Ganancia 
 Ganancia: real;
+
 begin
+//Iniciar array
+	estacionesStr[1]:= 'La montaña';
+	estacionesStr[2]:= 'La Aguada';
+	estacionesStr[3]:= 'Loma Redonda';
+	estacionesStr[4]:= 'Pico Espejo';
+	
 //Iniciar el contador
 contAsientosDisponibles:= 60;
 Ganancia:= 0;
   repeat
     // Menu Inicial
+    textcolor (green);
     writeln('|--------------------------------------|');
     writeln('|  Bienvenido al teleferico de Merida  |');
     writeln('|                                      |');
@@ -38,6 +53,7 @@ Ganancia:= 0;
       begin
       //Menu de compra
         ClrScr;
+        textcolor (lightblue);
         writeln('|--------------------------------------|');
         writeln('|                                      |');
         writeln('|     Ingrese su Nombre y Apellido     |');
@@ -45,6 +61,7 @@ Ganancia:= 0;
         writeln('|--------------------------------------|');
         readln(name);
         ClrScr;
+        textcolor (lightblue);
         writeln('|--------------------------------------|');
         writeln('|                                      |');
         writeln('|    Ingrese su Cedula de Identidad    |');
@@ -55,11 +72,23 @@ Ganancia:= 0;
         //Solicitar la informacion de cuantos boletos quiere comprar y de que tipo
         repeat
           writeln('|---------------------------------------|');
+          writeln('|   Estas en la estacion Barinitas      |');
+          writeln('|   Hasta que estacion quiere llegar?   |');
+          writeln('|    1. La montaña                      |');
+          writeln('|    2. La Aguada                       |');
+          writeln('|    3. Loma Redonda                    |');
+          writeln('|    4. Pico Espejo                     |');
+          writeln('|                                       |');
+          writeln('|---------------------------------------|');
+          readln(opMenuTramos);
+//Falta validacion
+          writeln('Ha selecconado la estacion ',estacionesStr[opMenuTramos],' por lo que recorrera ',opMenuTramos, ' estaciones');
+          writeln('|---------------------------------------|');
           writeln('|                                       |');
           writeln('|  Que tipo de Boletos desea comprar?   |');
-          writeln('|    1. Boleto General: 20$             |');
+          writeln('|    1. Boleto General: 5$ por tramo    |');
           writeln('|    2. Tercera edad y niños de         |');
-          writeln('|       3 a 12 años: 12$                |');
+          writeln('|       3 a 12 años: 3$ por tramo       |');
           writeln('|    3. Niños menores de 3 años         |');
           writeln('|       exonerados sin embargo, solo    |');
           writeln('|       pueden subir hasta Loma Redonda |');
@@ -69,9 +98,9 @@ Ganancia:= 0;
           readln(opMenuTipoBoletos);
           case (opMenuTipoBoletos) of
           '1':
-          precioboleto:= 20;
+          precioboleto:= 5;
           '2': 
-          precioboleto:= 12;
+          precioboleto:= 3;
           '3':
           precioboleto:= 0;
           '4':
@@ -89,6 +118,7 @@ Ganancia:= 0;
             begin
               writeln ('Ingrese una cantidad de boletos valida')
             end;
+          
           //ACA FALTA DE TODO UN POCO, FALTA SUMAR EL PRECIO, AGG UNA CONDICION DE QUE SI LA CANTIDAD DE ASIENTOS A COMPRAR ES 
           //MAYOR A A LOS DISPONIBLES DE ERROR, FALTA SUMAR EL DINERO Y PEDIR QUE ESTACION DESEA IR, CUALQUIER DUDA ME ESCRIBES
         until (opMenuTipoBoletos='4')
