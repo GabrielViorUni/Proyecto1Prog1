ac7e5573e8b76a8d24d671378259f03314745381
Isabella Garrido <igarrido.7420@unimar.edu.ve>
2024-10-24 19:53:11 -0400

    Menu de ventas y caracteres en espanol

--- Proyecto1.pas
+++ Proyecto1.pas
@@ -1,15 +1,14 @@
 program Proyecto1;
-
+{$codepage UTF8}
 uses crt,SysUtils;
 
 var
 //Areglos
 estacionesStr: array[1..4] of string;
-estaciones: array[1..4] of integer;
 //Menu inicial
 opMenuIni: string;
 //Contadores
-ContAsientosDisponibles: integer;
+totalAsientos: Integer;
 //Datos del Usuario
 name, CI: string;
 //Tramos
@@ -28,7 +27,9 @@ Ganancia: real;
 //Precio 1 Compra
 precio: real;
 precioAcumulado: real;
-
+//Menu ventas
+opMenuVentas: string;
+asientosDisponibles: real;
 begin
 //Iniciar array
 	estacionesStr[1]:= 'La montaña';
@@ -37,8 +38,8 @@ begin
 	estacionesStr[4]:= 'Pico Espejo';
 	
 //Iniciar el contador
-contAsientosDisponibles:= 60;
 Ganancia:= 0;
+totalAsientos:= 60;
 
 //Iniciar Variables
 precio:= 0;
@@ -48,9 +49,9 @@ precioAcumulado:= 0;
     // Menu Inicial
     textcolor (green);
     writeln('|--------------------------------------|');
-    writeln('|  Bienvenido al teleferico de Merida  |');
+    writeln('|  Bienvenido al Teleférico de Mérida  |');
     writeln('|                                      |');
-    writeln('|            Que desea hacer?          |');
+    writeln('|           ¿Qué desea hacer?          |');
     writeln('|    1. Comprar Boleto                 |');
     writeln('|    2. Ver estado de Ventas           |');
     writeln('|    3. Salir                          |');
@@ -74,7 +75,7 @@ precioAcumulado:= 0;
         textcolor (lightblue);
         writeln('|--------------------------------------|');
         writeln('|                                      |');
-        writeln('|    Ingrese su Cedula de Identidad    |');
+        writeln('|    Ingrese su Cédula de Identidad    |');
         writeln('|                                      |');
         writeln('|--------------------------------------|');      
         readln(CI);
@@ -83,8 +84,8 @@ precioAcumulado:= 0;
         repeat
         writeln('|---------------------------------------|');
         writeln('|   Estas en la estacion Barinitas      |');
-        writeln('|   Hasta que estacion quiere llegar?   |');
-        writeln('|    1. La monta',char(164),'a                      |');
+        writeln('|   ¿Hasta que estación quiere llegar?  |');
+        writeln('|    1. La montaña                      |');
         writeln('|    2. La Aguada                       |');
         writeln('|    3. Loma Redonda                    |');
         writeln('|    4. Pico Espejo                     |');
@@ -96,16 +97,16 @@ precioAcumulado:= 0;
         if((errorMenuTramos<>0) or ((opMenuTramos<1) or (opMenuTramos>4))) then
           writeln('|--------------------------------------|');
           writeln('|                                      |');
-          writeln('|       Ingrese una Opcion Valida      |');
+          writeln('|       Ingrese una Opción Válida      |');
           writeln('|                                      |');
           writeln('|--------------------------------------|');   
         until((errorMenuTramos=0) and ((opMenuTramos>0) and (opMenuTramos<5)));
         //Solicitar la informacion de cuantos boletos quiere comprar y de que tipo
         repeat
-          writeln('Ha selecconado la estacion ',estacionesStr[opMenuTramos],' por lo que recorrera ',opMenuTramos, ' estaciones');
+          writeln('Ha selecconado la estación ',estacionesStr[opMenuTramos],' por lo que recorrerá ',opMenuTramos, ' estaciones');
           writeln('|---------------------------------------|');
           writeln('|                                       |');
-          writeln('|  Que tipo de Boletos desea comprar?   |');
+          writeln('|  ¿Qué tipo de Boletos desea comprar?  |');
           writeln('|    1. Boleto General: 5$ por tramo    |');
           writeln('|    2. Tercera edad y niños de         |');
           writeln('|       3 a 12 años: 3$ por tramo       |');
@@ -127,27 +128,34 @@ precioAcumulado:= 0;
           '4':
           break;
           end;
-          repeat
-            writeln('|--------------------------------------|');
-            writeln('|                                      |');
-            writeln('|    Cuantos Boletos de este tipo      |');
-            writeln('|            desea comprar?            |');
-            writeln('|                                      |');
-            writeln('|--------------------------------------|');
-            readln(opMenuCantBoletosstr);
-            ClrScr;
-            val(opMenuCantBoletosstr, opMenuCantBoletosint, errorCantBoletos);
-            if (errorCantBoletos <> 0) then
-              begin
-                writeln('|--------------------------------------|');
-                writeln('|                                      |');
-                writeln('|     Ingrese una Cantidad Valida      |');
-                writeln('|                                      |');
-                writeln('|--------------------------------------|');   
-              end;
-          until(errorCantBoletos=0);
-          precio:= opMenuCantBoletosint*opMenuTramos*precioboleto;
-          precioAcumulado:= precioAcumulado+precio;
+            repeat
+                  writeln('|--------------------------------------|');
+                  writeln('|                                      |');
+                  writeln('|    ¿Cuántos Boletos de este tipo     |');
+                  writeln('|            desea comprar?            |');
+                  writeln('|                                      |');
+                  writeln('|--------------------------------------|');
+                  readln(opMenuCantBoletosstr);
+                ClrScr;
+                val(opMenuCantBoletosstr, opMenuCantBoletosint, errorCantBoletos);
+                if (errorCantBoletos <> 0) then
+              repeat     
+                  begin
+                    writeln('|--------------------------------------|');
+                    writeln('|                                      |');
+                    writeln('|     Ingrese una Cantidad Valida      |');
+                    writeln('|                                      |');
+                    writeln('|--------------------------------------|');   
+                  end;
+              until(errorCantBoletos=0);
+              precio:= opMenuCantBoletosint*opMenuTramos*precioboleto;
+              precioAcumulado:= precioAcumulado+precio;
+              if (opMenuCantBoletosint > totalAsientos) then
+                begin
+                  asientosDisponibles:= totalAsientos - opMenuCantBoletosint;
+                  writeln ('La cantidad de asientos solicitados excede el número de asientos disponibles');
+                end;
+            until (opMenuCantBoletosint < asientosDisponibles);
           //ACA FALTA DE TODO UN POCO, FALTA SUMAR EL PRECIO, AGG UNA CONDICION DE QUE SI LA CANTIDAD DE ASIENTOS A COMPRAR ES 
           //MAYOR A A LOS DISPONIBLES DE ERROR, FALTA SUMAR EL DINERO Y PEDIR QUE ESTACION DESEA IR, CUALQUIER DUDA ME ESCRIBES
         until (opMenuTipoBoletos='4');
@@ -158,7 +166,51 @@ precioAcumulado:= 0;
     //Ver estado de ventas
     '2':
       begin
-        writeln('')
+        repeat
+            textcolor (lightmagenta);
+            writeln('|---------------------------------------|');
+            writeln('|                                       |');
+            writeln('|            Estado de ventas           |');
+            writeln('|    ¿Qué desea ver?                    |');
+            writeln('|    1. Cantidad de boletos vendidos    |');
+            writeln('|    2. Asientos disponibles            |');
+            writeln('|    3. Reporte del día                 |');
+            writeln('|    4. Volver a menú inicial           |');
+            writeln('|---------------------------------------|');
+            readln(opMenuVentas);
+              case (opMenuVentas) of
+                '1':
+                begin
+                  textcolor (lightblue);
+                  writeln('|--------------------------------------|');
+                  writeln('|                                      |');
+                  writeln('|   La cantidad de boletos vendidos    |');
+                  writeln('|   Es:                                |');
+                  writeln('|                                      |');
+                  writeln('|--------------------------------------|');
+                end;
+                '2':
+                begin
+                  textcolor (lightblue);
+                  writeln('|--------------------------------------|');
+                  writeln('|                                      |');
+                  writeln('|  La cantidad de asientos disponibles |');
+                  writeln('|  Es:                                 |');
+                  writeln('|       ', asientosDisponibles:0:0,'                 |');
+                  writeln('|--------------------------------------|');
+                end; 
+                '3':
+                begin
+                  textcolor (lightblue);
+                  writeln('|--------------------------------------|');
+                  writeln('|                                      |');
+                  writeln('|  La ganancia del día es              |');
+                  writeln('|  Es:                                 |');
+                  writeln('|       ', Ganancia:0:0, '$','                            |');
+                  writeln('|--------------------------------------|');
+                end; 
+              end;  
+        until (opMenuVentas= '4');      
       end;
     //Salir
     '3': 
@@ -167,7 +219,7 @@ precioAcumulado:= 0;
     else
       begin
         ClrScr;
-        writeln('Ingrese una opcion valida');
+        writeln('Ingrese una opción valida');
       end;
       
     end;
