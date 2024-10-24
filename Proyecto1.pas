293beb6f30e4689dadb137e300363c99d69a9f05
Gabriel-Vivas <gvivas.9287@unimar.edu.ve>
2024-10-24 13:55:29 -0400

    Arreglos generales, sistema de ganancia y cobranza

--- Proyecto1.pas
+++ Proyecto1.pas
@@ -14,6 +14,8 @@ ContAsientosDisponibles: integer;
 name, CI: string;
 //Tramos
 opMenuTramos: Integer;
+opMenuTramosStr: String;
+errorMenuTramos: Integer;
 //Cantidad de boletos comprados 1 vez
 boletos: string;
 opMenuCantBoletosstr: string;
@@ -23,6 +25,9 @@ precioboleto: integer;
 errorCantBoletos: integer;
 //Ganancia 
 Ganancia: real;
+//Precio 1 Compra
+precio: real;
+precioAcumulado: real;
 
 begin
 //Iniciar array
@@ -34,6 +39,11 @@ begin
 //Iniciar el contador
 contAsientosDisponibles:= 60;
 Ganancia:= 0;
+
+//Iniciar Variables
+precio:= 0;
+precioAcumulado:= 0; 
+
   repeat
     // Menu Inicial
     textcolor (green);
@@ -69,19 +79,29 @@ Ganancia:= 0;
         writeln('|--------------------------------------|');      
         readln(CI);
         ClrScr;
+        //Solicitando a que estacion desea ir
+        repeat
+        writeln('|---------------------------------------|');
+        writeln('|   Estas en la estacion Barinitas      |');
+        writeln('|   Hasta que estacion quiere llegar?   |');
+        writeln('|    1. La monta',char(164),'a                      |');
+        writeln('|    2. La Aguada                       |');
+        writeln('|    3. Loma Redonda                    |');
+        writeln('|    4. Pico Espejo                     |');
+        writeln('|                                       |');
+        writeln('|---------------------------------------|');
+        readln(opMenuTramosStr);
+        Val(opMenuTramosStr,opMenuTramos,errorMenuTramos);
+        ClrScr;
+        if((errorMenuTramos<>0) or ((opMenuTramos<1) or (opMenuTramos>4))) then
+          writeln('|--------------------------------------|');
+          writeln('|                                      |');
+          writeln('|       Ingrese una Opcion Valida      |');
+          writeln('|                                      |');
+          writeln('|--------------------------------------|');   
+        until((errorMenuTramos=0) and ((opMenuTramos>0) and (opMenuTramos<5)));
         //Solicitar la informacion de cuantos boletos quiere comprar y de que tipo
         repeat
-          writeln('|---------------------------------------|');
-          writeln('|   Estas en la estacion Barinitas      |');
-          writeln('|   Hasta que estacion quiere llegar?   |');
-          writeln('|    1. La montaña                      |');
-          writeln('|    2. La Aguada                       |');
-          writeln('|    3. Loma Redonda                    |');
-          writeln('|    4. Pico Espejo                     |');
-          writeln('|                                       |');
-          writeln('|---------------------------------------|');
-          readln(opMenuTramos);
-//Falta validacion
           writeln('Ha selecconado la estacion ',estacionesStr[opMenuTramos],' por lo que recorrera ',opMenuTramos, ' estaciones');
           writeln('|---------------------------------------|');
           writeln('|                                       |');
@@ -96,6 +116,7 @@ Ganancia:= 0;
           writeln('|                                       |');
           writeln('|---------------------------------------|');
           readln(opMenuTipoBoletos);
+          ClrScr;
           case (opMenuTipoBoletos) of
           '1':
           precioboleto:= 5;
@@ -106,22 +127,33 @@ Ganancia:= 0;
           '4':
           break;
           end;
-          writeln('|--------------------------------------|');
-          writeln('|                                      |');
-          writeln('|    Cuantos Boletos de este tipo      |');
-          writeln('|            desea comprar?            |');
-          writeln('|                                      |');
-          writeln('|--------------------------------------|');
-          readln(opMenuCantBoletosstr);
-          val(opMenuCantBoletosstr, opMenuCantBoletosint, errorCantBoletos);
-          if (errorCantBoletos <> 0) then
-            begin
-              writeln ('Ingrese una cantidad de boletos valida')
-            end;
-          
+          repeat
+            writeln('|--------------------------------------|');
+            writeln('|                                      |');
+            writeln('|    Cuantos Boletos de este tipo      |');
+            writeln('|            desea comprar?            |');
+            writeln('|                                      |');
+            writeln('|--------------------------------------|');
+            readln(opMenuCantBoletosstr);
+            ClrScr;
+            val(opMenuCantBoletosstr, opMenuCantBoletosint, errorCantBoletos);
+            if (errorCantBoletos <> 0) then
+              begin
+                writeln('|--------------------------------------|');
+                writeln('|                                      |');
+                writeln('|     Ingrese una Cantidad Valida      |');
+                writeln('|                                      |');
+                writeln('|--------------------------------------|');   
+              end;
+          until(errorCantBoletos=0);
+          precio:= opMenuCantBoletosint*opMenuTramos*precioboleto;
+          precioAcumulado:= precioAcumulado+precio;
           //ACA FALTA DE TODO UN POCO, FALTA SUMAR EL PRECIO, AGG UNA CONDICION DE QUE SI LA CANTIDAD DE ASIENTOS A COMPRAR ES 
           //MAYOR A A LOS DISPONIBLES DE ERROR, FALTA SUMAR EL DINERO Y PEDIR QUE ESTACION DESEA IR, CUALQUIER DUDA ME ESCRIBES
-        until (opMenuTipoBoletos='4')
+        until (opMenuTipoBoletos='4');
+        writeln('El total a pagar es de ',precioAcumulado:0:0,'$');
+        Ganancia:= Ganancia+precioAcumulado;
+        precioAcumulado:= 0;
       end;
     //Ver estado de ventas
     '2':
@@ -140,4 +172,5 @@ Ganancia:= 0;
       
     end;
   until(opMenuIni='3');
+  writeln(Ganancia:0:0);
 end.
